29827bef45fcd2f60798b48819f7c252185c6404
“Shalini” <shalinibarai02@gmail.com>
2025-07-24 16:54:08 +0530

    auth complete

--- src/main/java/com/example/expenseReimbursement/repository/EmployeeRepository.java
+++ src/main/java/com/example/expenseReimbursement/repository/EmployeeRepository.java
@@ -1,7 +1,12 @@
 package com.example.expenseReimbursement.repository;
 
 import com.example.expenseReimbursement.entity.Employee;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+	boolean existsByEmail(String email);
+	Optional<Employee> findByEmail(String email);
 }
